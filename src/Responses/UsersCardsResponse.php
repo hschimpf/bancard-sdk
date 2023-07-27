002204f7e0b03389e54aaf5a8320285ccb30233c
Hermann D. Schimpf <hschimpf@hds-solutions.net>
2023-07-27 17:47:03 -0400

    FIX: Validate data received on UserCards response
    - If the request failed, there is no `cards` attribute on the response

--- src/Responses/UsersCardsResponse.php
+++ src/Responses/UsersCardsResponse.php
@@ -16,7 +16,7 @@ final class UsersCardsResponse extends Base\BancardResponse implements Contracts
 
     protected static function make(BancardRequest $request, object $data): self {
         // create an instance of every card
-        $cards = array_map(static fn($card) => new Card($request->user_id, $card), $data->cards);
+        $cards = array_map(static fn($card) => new Card($request->user_id, $card), $data->cards ?? []);
         // store user cards
         return new self($request, $cards);
     }
